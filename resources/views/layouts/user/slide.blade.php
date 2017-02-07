ff9a9ae8e89a92da8babf82f9aac26fbd592ea52
Thang <chukimthang94@gmail.com>
2017-02-07 19:04:20 +0700

    Static page

---
+++ resources/views/layouts/user/slide.blade.php
@@ -0,0 +1,32 @@
+<section id="featured">
+    <div class="container">
+        <div class="row">
+            <div id="wrapper_carousel" class="carousel slide col-lg-12" data-ride="carousel">
+                <div class="carousel-inner" role="listbox">
+                    <ol class="carousel-indicators">
+                        <li data-target="#wrapper_carousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#wrapper_carousel" data-slide-to="1"></li>
+                        <li data-target="#wrapper_carousel" data-slide-to="2"></li>
+                    </ol>
+                    <div class="item active">
+                        <img src="img/slides/1.jpg" alt="" />
+                    </div>
+                    <div class="item">
+                        <img src="img/slides/2.jpg" alt="" />
+                    </div>
+                    <div class="item">
+                        <img src="img/slides/3.jpg" alt="" />
+                    </div>
+                    <a class="left carousel-control" href="#wrapper_carousel" role="button" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#wrapper_carousel" role="button" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/user/home/home.blade.php
@@ -0,0 +1,141 @@
+@extends('layouts.user.app')
+
+@section('content')
+<section id="content">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h4 class="heading">Recent Works</h4>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <div class="box">
+                            <div class="box-gray aligncenter">
+                                <h4>Modern Style</h4>
+                                <div class="icon">
+                                    <img src="img/1.jpg" alt="">
+                                </div>
+                                <h5 align="center">Modern Style</h5>
+                            </div>
+                            <div class="box-bottom">
+                                <a href="#">Learn more</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3">
+                        <div class="box">
+                            <div class="box-gray aligncenter">
+                                <h4>Modern Style</h4>
+                                <div class="icon">
+                                    <img src="img/1.jpg" alt="">
+                                </div>
+                                <h5 align="center">Modern Style</h5>
+                            </div>
+                            <div class="box-bottom">
+                                <a href="#">Learn more</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3">
+                        <div class="box">
+                            <div class="box-gray aligncenter">
+                                <h4>Modern Style</h4>
+                                <div class="icon">
+                                    <img src="img/1.jpg" alt="">
+                                </div>
+                                <h5 align="center">Modern Style</h5>
+                            </div>
+                            <div class="box-bottom">
+                                <a href="#">Learn more</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3">
+                        <div class="box">
+                            <div class="box-gray aligncenter">
+                                <h4>Modern Style</h4>
+                                <div class="icon">
+                                    <img src="img/1.jpg" alt="">
+                                </div>
+                                <h5 align="center">Modern Style</h5>
+                            </div>
+                            <div class="box-bottom">
+                                <a href="#">Learn more</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+               
+        <div class="row">
+            <div class="col-lg-12">
+                <h4 class="heading">Recent Works</h4>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <div class="box">
+                            <div class="box-gray aligncenter">
+                                <h4>Modern Style</h4>
+                                <div class="icon">
+                                    <img src="img/1.jpg" alt="">
+                                </div>
+                                <h5 align="center">Modern Style</h5>
+                            </div>
+                            <div class="box-bottom">
+                                <a href="#">Learn more</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3">
+                        <div class="box">
+                            <div class="box-gray aligncenter">
+                                <h4>Modern Style</h4>
+                                <div class="icon">
+                                    <img src="img/1.jpg" alt="">
+                                </div>
+                                <h5 align="center">Modern Style</h5>
+                            </div>
+                            <div class="box-bottom">
+                                <a href="#">Learn more</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3">
+                        <div class="box">
+                            <div class="box-gray aligncenter">
+                                <h4>Modern Style</h4>
+                                <div class="icon">
+                                    <img src="img/1.jpg" alt="">
+                                </div>
+                                <h5 align="center">Modern Style</h5>
+                            </div>
+                            <div class="box-bottom">
+                                <a href="#">Learn more</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3">
+                        <div class="box">
+                            <div class="box-gray aligncenter">
+                                <h4>Modern Style</h4>
+                                <div class="icon">
+                                    <img src="img/1.jpg" alt="">
+                                </div>
+                                <h5 align="center">Modern Style</h5>
+                            </div>
+                            <div class="box-bottom">
+                                <a href="#">Learn more</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@stop
